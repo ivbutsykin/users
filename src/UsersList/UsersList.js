196db510e42e97fa396a17ab291aaa5d4872d21d
Ivan Butsykin <ivbutsykin@gmail.com>
2021-01-25 20:43:41 +0200

    Improving users list

--- src/UsersList/UsersList.js
+++ src/UsersList/UsersList.js
@@ -6,14 +6,14 @@ import Divider from '@material-ui/core/Divider';
 function UsersList(props) {
   return (
     <List>
-      {props.users.map(user => (
+      {props.users.map((user, index) => (
         <React.Fragment key={user.id}>
           <UsersListItem
             email={user.email}
             name={user.name}
             gender={user.gender}
           />
-          <Divider />
+          {index !== props.users.length - 1 && <Divider />}
         </React.Fragment>
       ))}
     </List>
