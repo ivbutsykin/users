428bd642442f3edf300e487a01549dc1a9b856c8
Ivan Butsykin <ivbutsykin@gmail.com>
2021-01-26 15:13:12 +0200

    Creating validation

--- src/CreateUserForm/CreateUserForm.js
+++ src/CreateUserForm/CreateUserForm.js
@@ -10,14 +10,92 @@ import Button from '@material-ui/core/Button';
 
 class CreateUserForm extends React.Component {
   state = {
-    email: '',
-    name: '',
-    gender: '',
+    fields: {
+      email: '',
+      name: '',
+      gender: '',
+    },
+    errors: {
+      email: false,
+      name: false,
+      gender: false,
+    },
+  };
+
+  handleClick = () => {
+    if (
+      this.validate(
+        this.state.fields.email,
+        this.state.fields.name,
+        this.state.fields.gender
+      )
+    ) {
+      this.props.onUserCreate(this.state.fields);
+      this.setState({
+        fields: {
+          email: '',
+          name: '',
+          gender: '',
+        },
+      });
+    }
   };
 
   handleChange = (e, stateType) => {
-    this.setState({ [stateType]: e.target.value });
+    this.setState(prevState => ({
+      fields: { ...prevState.fields, [stateType]: e.target.value },
+    }));
   };
+
+  validate = (email, name, gender) => {
+    this.setState({
+      errors: {
+        email: false,
+        name: false,
+        gender: false,
+      },
+    });
+
+    email = this.validateEmail(email);
+    name = this.validateName(name);
+    gender = this.validateGender(gender);
+
+    if (!email || !name || !gender) {
+      if (!email) {
+        this.setState(prevState => ({
+          errors: { ...prevState.errors, email: true },
+        }));
+      }
+
+      if (!name) {
+        this.setState(prevState => ({
+          errors: { ...prevState.errors, name: true },
+        }));
+      }
+
+      if (!gender) {
+        this.setState(prevState => ({
+          errors: { ...prevState.errors, gender: true },
+        }));
+      }
+      return false;
+    }
+    return true;
+  };
+
+  validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  validateName(name) {
+    const re = /^[A-Za-z\s]+$/;
+    return re.test(name);
+  }
+
+  validateGender(gender) {
+    return gender ? true : false;
+  }
 
   render() {
     return (
@@ -27,9 +105,13 @@ class CreateUserForm extends React.Component {
             <Grid item xs={12}>
               <FormControl required fullWidth>
                 <InputLabel>Email</InputLabel>
-                <Input onChange={e => this.handleChange(e, 'email')}></Input>
+                <Input
+                  value={this.state.fields.email}
+                  error={this.state.errors.email}
+                  onChange={e => this.handleChange(e, 'email')}
+                ></Input>
               </FormControl>
-              <FormHelperText>Required</FormHelperText>
+              <FormHelperText>user@example.com</FormHelperText>
             </Grid>
           </Grid>
         </Grid>
@@ -38,9 +120,13 @@ class CreateUserForm extends React.Component {
             <Grid item xs={12}>
               <FormControl required fullWidth>
                 <InputLabel>Name</InputLabel>
-                <Input onChange={e => this.handleChange(e, 'name')}></Input>
+                <Input
+                  value={this.state.fields.name}
+                  error={this.state.errors.name}
+                  onChange={e => this.handleChange(e, 'name')}
+                ></Input>
               </FormControl>
-              <FormHelperText>Required</FormHelperText>
+              <FormHelperText>Letters a-z, A-Z</FormHelperText>
             </Grid>
           </Grid>
         </Grid>
@@ -50,24 +136,22 @@ class CreateUserForm extends React.Component {
               <FormControl required fullWidth>
                 <InputLabel>Gender</InputLabel>
                 <Select
-                  value={this.state.gender}
+                  error={this.state.errors.gender}
+                  value={this.state.fields.gender}
                   onChange={e => this.handleChange(e, 'gender')}
                 >
                   <MenuItem value={'Male'}>Male</MenuItem>
                   <MenuItem value={'Female'}>Female</MenuItem>
                 </Select>
               </FormControl>
-              <FormHelperText>Required</FormHelperText>
+              <FormHelperText>Male or Female</FormHelperText>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
           <Grid container justify="center">
             <Grid item>
-              <Button
-                variant="contained"
-                onClick={() => this.props.onUserCreate(this.state)}
-              >
+              <Button variant="contained" onClick={this.handleClick}>
                 Add
               </Button>
             </Grid>
